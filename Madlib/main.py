0e2fec106abe53eb74be4e333d764052d3690d58
ThatTechGuy <rmartin@fullsail.edu>
2015-07-09 22:46:48 -0400

    Array, Loop, Function Forming

---
+++ Madlib/main.py
@@ -0,0 +1,37 @@
+"""
+Name: Rick Martin
+Class: DPW1507
+Assignment: Madlib
+"""
+
+print ""
+
+questions = [
+    "What is your favorite color? ",
+    "What state do you live in? ",
+    "Do you have any children? ",
+    "How old where you in 1990? ",
+    "How much does a wood chuck chuck? "
+]
+
+def validate():
+    if answers[0] == red:
+        
+
+
+answers = []
+
+for question in questions:
+    answer = raw_input(question).lower()
+    answers.append(answer)
+    validate()
+
+def kids():
+	det['kid'] = raw_input("Well do ya or don't ya? ").lower()
+	if det['kid'] == "yes" or det_kid == "y":
+		madlib =
+
+
+child
+friend
+spouse
