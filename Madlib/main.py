3b58eccf2e71b21958d2d881286c9cf2fe4fb109
ThatTechGuy <rmartin@fullsail.edu>
2015-07-09 23:13:01 -0400

    Cleaning Up and Validating

--- Madlib/main.py
+++ Madlib/main.py
@@ -4,7 +4,7 @@ Class: DPW1507
 Assignment: Madlib
 """
 
-print ""
+print "Welcome my friend, I am a fortune teller, answer my questions and I will tell you your future!"
 
 questions = [
     "What is your favorite color? ",
@@ -14,24 +14,29 @@ questions = [
     "How much does a wood chuck chuck? "
 ]
 
-def validate():
-    if answers[0] == red:
-        
-
-
+madlib = {}
 answers = []
+
+def validate():
+    if answers[0] == "red":
+        madlib['mood'] = "angered"
+    elif answers[0] == "blue":
+        madlib['mood'] = "saddened"
+    elif answers[0] == "green":
+        madlib['mood'] = "envious"
+    elif answers[0] == "yellow":
+        madlib['mood'] == "elated"
+    else:
+        madlib['mood'] == "confused"
+    if answers[2] == "yes" or "y":
+        madlib['kids'] == raw_input("How many kids do you have? ")
+    elif answers[2] == "no" or "n":
+        madlib['kids'] == "no"
+    else:
+        answers[2] == raw_input("Seriously, do you have any kids? ")
+        validate()
 
 for question in questions:
     answer = raw_input(question).lower()
     answers.append(answer)
     validate()
-
-def kids():
-	det['kid'] = raw_input("Well do ya or don't ya? ").lower()
-	if det['kid'] == "yes" or det_kid == "y":
-		madlib =
-
-
-child
-friend
-spouse
