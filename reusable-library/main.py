d7d9f6a8ca58e8b910db9cba88f540fc8557a292
ThatTechGuy <rmartin@fullsail.edu>
2015-07-23 11:40:48 -0400

    Added Reusable Library GAEL

---
+++ reusable-library/main.py
@@ -0,0 +1,15 @@
+'''
+Name: Rick Martin
+Class: DPW1507
+Assignment: Reusable Library
+'''
+
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
