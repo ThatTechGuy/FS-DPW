87f38a6539bcd01fbfc6f293e940eb92ad9bf888
ThatTechGuy <rmartin@fullsail.edu>
2015-07-23 16:56:27 -0400

    HTML Form Structure

--- reusable-library/pages.py
+++ reusable-library/pages.py
@@ -31,7 +31,64 @@ class DefaultPage(object):
 '''
         self.section = '''
     <section>
-        Here we are.
+      <div class="container">
+        <div class="page-header clearfix">
+          <h1 class="pull-left">Record New Business</h1>
+        </div>
+        <div class="jumbotron">
+          <form class="form-horizontal" method="GET">
+            <div class="form-group">
+              <label for="name" class="col-sm-2 control-label">Business Name</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="name" name="name" placeholder="John Doe's Lawn Care" />
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="type" class="col-sm-2 control-label">Business Type</label>
+              <div class="col-sm-10">
+                <select class="form-control" id="type" name="type">
+                  <option value="DBA">Doing Business As (DBA)</option>
+                  <option value="LLC">Limited Liability Company (LLC)</option>
+                  <option value="INC">Incorporated Company (INC)</option>
+                </select>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="contact" class="col-sm-2 control-label">Contact Name</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="contact" name="contact" placeholder="John Doe" />
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="number" class="col-sm-2 control-label">Contact Number</label>
+              <div class="col-sm-10">
+                <input type="tel" class="form-control" id="number" name="number" placeholder="1234567890" />
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="files" class="col-sm-2 control-label">Files Available</label>
+              <div class="col-sm-10">
+                <label class="checkbox-inline">
+                  <input type="checkbox" id="sunbiz" name="files[sun]" value="1"> Sunbiz Registration
+                </label>
+                <label class="checkbox-inline">
+                  <input type="checkbox" id="articles" name="files[art]" value="1"> Articles of Incorporation
+                </label>
+                <label class="checkbox-inline">
+                  <input type="checkbox" id="workcomp" name="files[wcs]" value="1"> Workman's Comp Search
+                </label>
+                <label class="checkbox-inline">
+                  <input type="checkbox" id="irsletter" name="files[irs]" value="1"> IRS Letter w/ Tax ID
+                </label>
+                <label class="checkbox-inline">
+                  <input type="checkbox" id="custloa" name="files['loa']" value="1"> Letter of Authority
+                </label>
+              </div>
+            </div>
+            <input type="submit" class="btn btn-primary btn-block" value="Save Business Record" />
+          </form>
+        </div>
+      </div>
     </section>
 '''
         self.footer = '''
