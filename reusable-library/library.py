fa430ae097e3e42835b70cd43efba511eb0e19a5
ThatTechGuy <rmartin@fullsail.edu>
2015-07-23 23:02:16 -0400

    Preparing Validation

--- reusable-library/library.py
+++ reusable-library/library.py
@@ -10,6 +10,7 @@ class Record(object):
     def record(self,form):
         self.__record = form
         self.check_eligibility()
+        self.list_item()
 
     def check_eligibility(self):
         if all (k in self.__record for k in ("sun","art","wcs","irs","loa")):
@@ -29,27 +30,27 @@ class Record(object):
             self.__record['color'] = self.bootstrap_color("red")
 
     def list_item(self):
-        for key in self.__record:
-            if key == "sun":
-                self.__record['lg_sun'] = '''
-                <li class="list-group-item">Sunbiz Registration</li>
-                '''
-            if key == "":
-                self.__record['lg_art'] = '''
-                <li class="list-group-item">Articles of Incorporation</li>
-                '''
-            if key == "sun":
-                self.__record['lg_wcs'] = '''
-                <li class="list-group-item">Workman's Comp Search</li>
-                '''
-            if key == "sun":
-                self.__record['lg_irs'] = '''
-                <li class="list-group-item">IRS Letter w/ Tax ID</li>
-                '''
-            if key == "sun":
-                self.__record['lg_loa'] = '''
-                <li class="list-group-item">Letter of Authority</li>
-                '''
+        self.__record['files'] = ""
+        if 'sun' in self.__record:
+            self.__record['files'] += '''
+            <li class="list-group-item">Sunbiz Registration</li>
+            '''
+        if 'art' in self.__record:
+            self.__record['files'] += '''
+            <li class="list-group-item">Articles of Incorporation</li>
+            '''
+        if 'wcs' in self.__record:
+            self.__record['files'] += '''
+            <li class="list-group-item">Workman's Comp Search</li>
+            '''
+        if 'irs' in self.__record:
+            self.__record['files'] += '''
+            <li class="list-group-item">IRS Letter w/ Tax ID</li>
+            '''
+        if 'loa' in self.__record:
+            self.__record['files'] += '''
+            <li class="list-group-item">Letter of Authority</li>
+            '''
 
     def bootstrap_color(self,color):
         if color == "green":
