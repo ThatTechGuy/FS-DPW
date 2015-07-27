ab6ed57cecf93a6098f34bef845a51cbd61291ba
ThatTechGuy <rmartin@fullsail.edu>
2015-07-27 15:42:14 -0400

    Reflow Controller Setup

--- dynamic-site/main.py
+++ dynamic-site/main.py
@@ -11,10 +11,17 @@ from lib import *
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        this = DataTrojan()
-        compiler = DataCompiler
-        compiler.push = this.data
+        #Instantiate compiler with DataCompiler from library
+        compiler = DataCompiler()
+        #Instantiate multiple DataTrojans from data and pass to compiler through setter
+        compiler.push = DataTrojan("who")
+        compiler.push = DataTrojan("what")
+        compiler.push = DataTrojan("when")
+        compiler.push = DataTrojan("where")
+        compiler.push = DataTrojan("why")
+        #Instantiate HomePage from page and pass from compiler through getter
         display = HomePage(compiler.pull)
+        #Write loaded and compiled page from HomePage class function load()
         self.response.write(display.load())
 
 app = webapp2.WSGIApplication([
