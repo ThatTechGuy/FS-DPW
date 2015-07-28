30a8e1c59b5886c04a301b6da7115e057fdf4b20
ThatTechGuy <rmartin@fullsail.edu>
2015-07-27 21:02:26 -0400

    Created Data Functions

--- dynamic-site/data.py
+++ dynamic-site/data.py
@@ -16,16 +16,19 @@ class DataTrojan(object):
         ["Echo","Iraq","Taurus","Spitfire",brief[4]]
     ]
 
+    @property
+    def data(self):
+        op_names = []
+        for ops in self.__database:
+            op_names.append(ops[0])
+        del op_names[0]
+        return op_names
 
-'''
-DataTrojan
-
-noparam
-Page Array
-
-withparam
-name
-local
-squad
-leader
-brief
+    def auth(self, op):
+        op_data = {}
+        search = (ops for ops in self.__database if ops[0] == op).next()
+        i = 0
+        for key in self.__database[0]:
+            op_data[key] = search[i]
+            i += 1
+        return op_data
